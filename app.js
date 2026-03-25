887ea6a619123d6be35010774bae1e00c4e5ba8d
Stiehl <89173744+stiehl122@users.noreply.github.com>
2026-03-25 14:43:29 -0400

    chore(release): bump APP_VERSION to v0.6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ─── CONFIG ───────────────────────────────────────────────────────────────────
-const APP_VERSION = 'v0.5.5';
+const APP_VERSION = 'v0.6';
 const IS_PREVIEW = window.location.hostname.endsWith('.vercel.app') &&
   window.location.hostname !== 'el-roys-drink-menu.vercel.app';
 
